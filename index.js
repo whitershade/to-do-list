1a582dd08f4dba16033bb4b21c580cca04cffb45
Alex Prokopenko <alex.r@didww.com>
2018-06-17 15:13:39 +0300

    add PATCH /todos/:id route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ObjectID } = require('mongodb');
+const { pick } = require('lodash');
 
 const Todo = require('./models/todo');
 // const User = require('./models/user');
@@ -11,11 +12,11 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.post('/todos', (req, res) => {
-  const todo = new Todo({
+  const newTodo = new Todo({
     text: req.body.text,
   });
 
-  todo
+  newTodo
     .save()
     .then((doc) => {
       res.send(doc);
@@ -39,9 +40,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   const { params: { id } } = req;
 
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
+  if (!ObjectID.isValid(id)) return res.status(404).send();
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
@@ -61,6 +60,31 @@ app.delete('/todos/:id', (req, res) => {
 
   Todo
     .findByIdAndRemove(id)
+    .then((todo) => {
+      if (!todo) return res.status(404).send();
+
+      res.send({ todo });
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const { params: { id } } = req;
+
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  const body = pick(req.body, ['text', 'completed']);
+
+  if (body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completedAt = null;
+  }
+
+  Todo
+    .findByIdAndUpdate(id, { $set: body }, { new: true })
     .then((todo) => {
       if (!todo) return res.status(404).send();
 
