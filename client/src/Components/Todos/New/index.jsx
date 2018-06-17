51c2c3bc5ab69894a19f3c64e30561f675db4f96
Alex Prokopenko <alex.r@didww.com>
2018-06-17 20:43:01 +0300

    added new todo form styles

--- client/src/Components/Todos/New/index.jsx
+++ client/src/Components/Todos/New/index.jsx
@@ -1,33 +1,52 @@
 import React from "react";
 import { Form, Field } from "react-final-form";
+import TextField from '@material-ui/core/TextField';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import './styles.css';
 
 const required = value => (value ? undefined : "Required");
 
 const Component = ({ onSubmit }) => (
   <Form
+    styleName='form'
     onSubmit={onSubmit}
     render={({ handleSubmit, submitting, pristine, values, form: { reset } }) => (
       <form onSubmit={handleSubmit}>
         <Field name="text" validate={required}>
           {({ input, meta }) => (
             <div>
-              <label>Text</label>
-              <input {...input} type="text" placeholder="First Name" />
-              {meta.error && meta.touched && <span>{meta.error}</span>}
+              <FormControl>
+                <TextField
+                  label="Text"
+                  error={ meta.error && meta.touched  }
+                  {...input}
+                  placeholder="Enter text"
+                />
+                { meta.error && meta.touched ? <FormHelperText>{ meta.error }</FormHelperText> : null }
+              </FormControl>
             </div>
           )}
         </Field>
         <div className="buttons">
-          <button type="submit" disabled={submitting}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={submitting}
+          >
             Submit
-          </button>
-          <button
+          </Button>
+          <Button
             type="button"
+            variant="contained"
+            color="secondary"
             onClick={reset}
             disabled={submitting || pristine}
           >
             Reset
-          </button>
+          </Button>
         </div>
       </form>
     )}
