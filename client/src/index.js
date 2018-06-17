a94376578b85fcadbb7ad975068a03a21ec37aed
Alex Prokopenko <alex.r@didww.com>
2018-06-17 17:35:19 +0300

    added reducers initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import getStore from './getStore';
+import App from './Components/App';
+import reducers from './Reducers';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+
+const store = getStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
