761dd6d18a3e3f1aab1ae80c14fcd47b6a5ed4d7
Alex Prokopenko <alex.r@didww.com>
2018-06-17 12:59:25 +0300

    setup app db for heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise; // Use native promises instead of mpromise
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = mongoose;
